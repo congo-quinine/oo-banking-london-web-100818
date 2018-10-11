366525c3433d663380c2883ca2999c097f58b03e
Rob Parker <into.the.parker@gmail.com>
2018-10-11 15:44:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
+require_relative '../lib/bank_account'
+
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :receiver, :status, :amount
+  @@history = []
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+  
+  def valid?
+    @sender.valid?
+    @receiver.valid?
+  end
+  
+  def execute_transaction
+    if @sender.valid? == true
+    @sender.balance -= @amount 
+    @receiver.balance += @amount
+    @status = 'complete'
+    elsif @sender.valid? == false
+    @status = "Transaction rejected. Please check your account balance."
+    end
+  end
+  
 end
